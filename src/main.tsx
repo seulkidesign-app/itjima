24b2d9f69354d267ccca3ee6a09f661771ecda48
seulkidesign-app <seulki.design@gmail.com>
2026-07-27 11:26:31 +0900

    Load refined fields and states system

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import "./ui-interactions.css";
 import "./ui-layout-system.css";
 import "./ui-design-qa.css";
 import "./ui-controls-system.css";
+import "./ui-fields-and-states.css";
 
 import { registerServiceWorker } from "@/lib/swReminders";
 
