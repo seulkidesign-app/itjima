c75e2f5655aa41b48b76ca0445786030ae07f6a7
seulkidesign-app <seulki.design@gmail.com>
2026-08-01 00:35:44 +0900

    Load the professional responsive layout layer

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,7 @@ import "./ui-controls-system.css";
 import "./ui-fields-and-states.css";
 import "./ui-premium.css";
 import "./ui-wwdc-quality.css";
+import "./ui-responsive-pro.css";
 
 import { registerServiceWorker } from "@/lib/swReminders";
 import { installPushSubscriptionAuthSync } from "@/lib/push/pushAuthSync";
