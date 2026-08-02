ca3aeea4bc24fe5b90a06fbcc9bf08a48db944da
seulkidesign-app <seulki.design@gmail.com>
2026-08-02 11:28:35 +0900

    fix: load the mobile composer layout contract last

--- src/main.tsx
+++ src/main.tsx
@@ -26,6 +26,7 @@ import "./ui-adaptive-navigation.css";
 import "./ui-mobile-light-contract.css";
 import "./ui-layout-viewport-polish.css";
 import "./ui-calendar-experience.css";
+import "./ui-home-fixed-composer.css";
 
 import { registerServiceWorker } from "@/lib/swReminders";
 import { installPushSubscriptionAuthSync } from "@/lib/push/pushAuthSync";
