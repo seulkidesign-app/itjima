0e4fe2db65dbf8ebc332a60a47ea96301d10cfd6
seulkidesign-app <seulki.design@gmail.com>
2026-08-16 20:10:47 +0900

    style: load landing footer and headline polish

--- src/main.tsx
+++ src/main.tsx
@@ -31,6 +31,7 @@ import "./ui-schedule-final-polish.css";
 import "./ui-landing-scroll-fix.css";
 import "./ui-landing-interactions.css";
 import "./ui-landing-brand-system.css";
+import "./ui-landing-footer-headline-polish.css";
 import "./ui-pwa-install-experience.css";
 import "./ui-interaction-craft.css";
 
